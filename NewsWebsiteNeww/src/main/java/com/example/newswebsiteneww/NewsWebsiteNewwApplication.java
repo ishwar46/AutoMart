9e1696b0aa81720509de7a3c2ccb45507f2cda31
ishwar46 <ishwarura@gmail.com>
2023-01-23 19:50:51 +0545

    Update
    Spring Boot  Added

---
+++ NewsWebsiteNeww/src/main/java/com/example/newswebsiteneww/NewsWebsiteNewwApplication.java
@@ -0,0 +1,13 @@
+package com.example.newswebsiteneww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebsiteNewwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWebsiteNewwApplication.class, args);
+	}
+
+}
---
+++ NewsWebsiteNeww/src/test/java/com/example/newswebsiteneww/NewsWebsiteNewwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newswebsiteneww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebsiteNewwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
