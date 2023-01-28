18a25ae4e8a568ec93e5a41d4d5cc8b752c5b07c
ishwar46 <ishwarura@gmail.com>
2023-01-28 18:56:22 +0545

    update

--- js/app.js
+++ js/app.js
@@ -65,3 +65,43 @@ const workSecObserver = new IntersectionObserver(workSectionObserve, {
 });
 
 workSecObserver.observe(workSection);
+
+
+// ========================================
+//  how to add media queries in JS
+// ========================================
+function myFunction(widthSize) {
+    if (widthSize.matches) {
+        // If media query matches
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    } else {
+        const swiper = new Swiper(".swiper", {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    }
+}
+
+const widthSize = window.matchMedia("(max-width: 780px)");
+// Call listener function at run time
+myFunction(widthSize);
