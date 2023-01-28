fad9d2a5809da879f7a0e989b0a21ea6e17f804a
ishwar46 <ishwarura@gmail.com>
2023-01-28 17:39:17 +0545

    adding sections

--- js/app.js
+++ js/app.js
@@ -16,6 +16,52 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 //Image Slider
+
+
+// ========================================
+//  animated counter number
+// ========================================
+
+const workSection = document.querySelector(".section-work-data");
+
+const workSectionObserve = (entries) => {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    console.log(entries);
+
+
+    const counterNum = document.querySelectorAll(".counter-numbers");
+    // console.log(counterNum);
+    const speed = 200;
+
+    counterNum.forEach((curNumber) => {
+        const updateNumber = () => {
+            const targetNumber = parseInt(curNumber.dataset.number);
+            // console.log(targetNumber);
+            const initialNumber = parseInt(curNumber.innerText);
+            // console.log(initialNumber);
+            const incrementNumber = Math.trunc(targetNumber / speed);
+            // i am adding the value to the main number
+            // console.log(incrementNumber);
+
+            if (initialNumber < targetNumber) {
+                curNumber.innerText = `${initialNumber + incrementNumber}+`;
+                setTimeout(updateNumber, 10);
+            } else {
+                curNumber.innerText = `${targetNumber}+`;
+            }
+
+        };
+        updateNumber();
+    });
+};
+
+const workSecObserver = new IntersectionObserver(workSectionObserve, {
+    root: null,
+    threshold: 0,
+});
+
+workSecObserver.observe(workSection);
