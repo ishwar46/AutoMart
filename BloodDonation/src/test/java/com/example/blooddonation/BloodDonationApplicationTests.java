62b8659b756ab1e202a1571b6060ea15c6ab8b39
ishwar46 <ishwarura@gmail.com>
2023-02-01 07:24:01 +0545

    Project Added

---
+++ BloodDonation/src/test/java/com/example/blooddonation/BloodDonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blooddonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
