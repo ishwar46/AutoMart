3dbebc3396fb767165ea4b858a2beee1579c25fa
ishwar46 <ishwarura@gmail.com>
2023-02-07 21:14:35 +0545

    update

--- src/main/java/com/example/bookmarts/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package  com.example.automart.repository;
-
-import  com.example.automart.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryName(String name);
-}
